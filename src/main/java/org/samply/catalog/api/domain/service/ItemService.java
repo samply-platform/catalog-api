20ae51c9faa2a0c5d6207884ef2570807003c3c3
frederik.steffen <frederik.steffen@gmail.com>
2019-11-29 15:26:50 +0100

    some update checks, added graphql client lib

--- src/main/java/org/samply/catalog/api/domain/service/ItemService.java
+++ src/main/java/org/samply/catalog/api/domain/service/ItemService.java
@@ -1,21 +1,27 @@
 package org.samply.catalog.api.domain.service;
 
+import static io.micronaut.http.HttpStatus.FORBIDDEN;
+import static io.micronaut.http.HttpStatus.NOT_FOUND;
 import java.util.UUID;
 import javax.inject.Singleton;
 import org.samply.catalog.api.domain.model.Item;
-import org.samply.catalog.api.domain.model.ItemDataDTO;
 import org.samply.catalog.api.domain.model.ItemDTO;
+import org.samply.catalog.api.domain.model.ItemDataDTO;
 import org.samply.catalog.api.domain.model.ItemId;
 import org.samply.catalog.api.domain.model.SellerId;
+import org.samply.catalog.api.web.CatalogQueryApiClient;
+import io.micronaut.http.exceptions.HttpStatusException;
 import io.reactivex.Single;
 
 @Singleton
 public class ItemService {
 
     private final ItemPublisher itemPublisher;
+    private final CatalogQueryApiClient catalogQueryApiClient;
 
-    public ItemService(ItemPublisher itemPublisher) {
+    public ItemService(ItemPublisher itemPublisher, CatalogQueryApiClient catalogQueryApiClient) {
         this.itemPublisher = itemPublisher;
+        this.catalogQueryApiClient = catalogQueryApiClient;
     }
 
     public Single<ItemDTO> addItem(ItemDataDTO item, SellerId sellerId) {
@@ -23,7 +29,11 @@ public class ItemService {
     }
 
     public Single<ItemDTO> updateItem(ItemId itemId, ItemDataDTO item, SellerId sellerId) {
-        return publishItem(itemId, item, sellerId);
+        return catalogQueryApiClient.getItem(itemId)
+                                    .switchIfEmpty(Single.error(new HttpStatusException(NOT_FOUND, "Not Found.")))
+                                    .filter(response -> sellerId.equals(response.getSellerId()))
+                                    .switchIfEmpty(Single.error(new HttpStatusException(FORBIDDEN, "Forbidden.")))
+                                    .flatMap(i -> publishItem(itemId, item, sellerId));
     }
 
     private Single<ItemDTO> publishItem(ItemDataDTO item, SellerId sellerId) {
